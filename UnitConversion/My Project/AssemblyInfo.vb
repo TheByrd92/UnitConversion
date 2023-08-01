64b07db91f6cd3af38dc7a115522774724bd33a9
TheByrd92 <iByrd92@yahoo.com>
2023-08-01 14:12:17 -0500

    Upgrading this to .NET 7

--- UnitConversion/My Project/AssemblyInfo.vb
+++ UnitConversion/My Project/AssemblyInfo.vb
@@ -1,35 +1,9 @@
 ﻿Imports System
 Imports System.Reflection
 Imports System.Runtime.InteropServices
-
-' General Information about an assembly is controlled through the following
-' set of attributes. Change these attribute values to modify the information
-' associated with an assembly.
-
-' Review the values of the assembly attributes
-
-<Assembly: AssemblyTitle("UnitConversion")>
-<Assembly: AssemblyDescription("")>
-<Assembly: AssemblyCompany("Hewlett-Packard Company")>
-<Assembly: AssemblyProduct("UnitConversion")>
-<Assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2018")>
 <Assembly: AssemblyTrademark("")>
 
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
 <Assembly: Guid("6fa599fa-b555-4de5-b269-45870e6717a7")>
-
-' Version information for an assembly consists of the following four values:
-'
-'      Major Version
-'      Minor Version
-'      Build Number
-'      Revision
-'
-' You can specify all the values or you can default the Build and Revision Numbers
-' by using the '*' as shown below:
-' <Assembly: AssemblyVersion("1.0.*")>
-
-<Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
